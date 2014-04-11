2cc18bfe8d2d54c7ad9d547c82e8be6e230d7a7e
Mat Doidge <hello@visualdecree.co.uk>
2014-04-11 19:03:19 +0100

    define correct paths in config files

--- wp-config.php
+++ wp-config.php
@@ -14,24 +14,35 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+if ($_SERVER['REMOTE_ADDR']=='127.0.0.1') {
+    define('WP_ENV', 'development');
+} else {
+    define('WP_ENV', 'production');
+}
 
-/** MySQL database username */
-define('DB_USER', 'username_here');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+// MySQL settings - You can get this info from your web host //
+if (WP_ENV == 'development') {
+    define('DB_NAME', 'mattdingle');
+    define('DB_USER', 'mattdingle');
+    define('DB_PASSWORD', 'Redeemed123');
+    define('DB_HOST', 'localhost');
+} else {
+    define('DB_NAME', 'mydb-prod');
+    define('DB_USER', 'username');
+    define('DB_PASSWORD', 'pasdword');
+    define('DB_HOST', 'mysql.mysite.com');
+} 
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
+
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
+define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
 
 /**#@+
  * Authentication Unique Keys and Salts.
